8c2f1743b6c5efc81139f30575f80e17d70e00d9
JVRC22 <javier.res220704@gmail.com>
2023-02-17 00:47:26 -0600

    Funciones usuario

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -157,10 +157,279 @@ class UsersController extends Controller
     }
 
     //Funciones de Administrador
-    public function cambiarRol()
-    {}
+    public function mostrarUsuarios()
+    {
+        $usuarios = User::all();
+
+        return $usuarios;
+    }
+
+    public function eliminarUsuario($id)
+    {
+        $usuario = User::find($id);
+
+        if($usuario)
+        {
+            if($usuario->delete())
+            {
+                return response()->json([
+                    'message' => 'Usuario eliminado'
+                ], 200);
+            }
+
+            else
+            {
+                return response()->json([
+                    'message' => 'Error al eliminar el usuario'
+                ], 500);
+            }
+        }
+
+        else
+        {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+    }
+
+    public function cambiarRol(Request $request, $id)
+    {
+        $usuario = User::find($id);
+
+        if($usuario)
+        {
+            $validacion = Validator::make(
+                $request->all(),
+                [
+                    'role' => 'required|integer',
+                ],
+                [
+                    'role.required' => 'El rol es requerido',
+                    'role.integer' => 'El rol debe ser un número entero',
+                ]
+            );
+
+            if ($validacion->fails()) {
+                return response()->json([
+                    'message' => $validacion->errors(),
+                ], 400);
+            }
+
+            else
+            {
+                $usuario->rol = $request->rol;
+
+                if($usuario->save())
+                {
+                    return response()->json([
+                        'message' => 'Rol cambiado'
+                    ], 200);
+                }
+
+                else
+                {
+                    return response()->json([
+                        'message' => 'Error al cambiar el rol'
+                    ], 500);
+                }
+            }
+        }
+
+        else
+        {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+    }
+
+    public function cambiarStatus(Request $request, $id)
+    {
+        $usuario = User::find($id);
+
+        if($usuario)
+        {
+            $validacion = Validator::make(
+                $request->all(),
+                [
+                    'status' => 'required|integer',
+                ],
+                [
+                    'status.required' => 'El status es requerido',
+                    'status.integer' => 'El status debe ser un número entero',
+                ]
+            );
+
+            if ($validacion->fails()) {
+                return response()->json([
+                    'message' => $validacion->errors(),
+                ], 400);
+            }
+
+            else
+            {
+                $usuario->status = $request->status;
+
+                if($usuario->save())
+                {
+                    return response()->json([
+                        'message' => 'Status cambiado'
+                    ], 200);
+                }
+
+                else
+                {
+                    return response()->json([
+                        'message' => 'Error al cambiar el status'
+                    ], 500);
+                }
+            }
+        }
+
+        else
+        {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+    }
 
     //Funciones de Usuario
-    public function cambiarPassword()
-    {}
+    public function mostrarUsuarioUnico($id)
+    {
+        $usuario = User::find($id);
+
+        if($usuario)
+        {
+            return $usuario;
+        }
+
+        else
+        {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+    }
+
+    public function cambiarNombre(Request $request, $id)
+    {
+        $usuario = User::find($id);
+
+        if($usuario)
+        {
+            $validacion = Validator::make(
+                $request->all(),
+                [
+                    'name' => 'required|string|max:255'
+                ],
+                [
+                    'name.required' => 'El nombre es requerido',
+                    'name.string' => 'El nombre debe ser una cadena de texto',
+                    'name.max' => 'El nombre debe tener como máximo 255 caracteres',
+                ]
+            );
+
+            if ($validacion->fails()) {
+                return response()->json([
+                    'message' => $validacion->errors(),
+                ], 400);
+            }
+
+            else
+            {
+                $usuario->name = $request->name;
+
+                if($usuario->save())
+                {
+                    return response()->json([
+                        'message' => 'Cuenta editada'
+                    ], 200);
+                }
+
+                else
+                {
+                    return response()->json([
+                        'message' => 'Error al editar la cuenta'
+                    ], 500);
+                }
+            }
+        }
+
+        else
+        {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+    }
+
+    public function cambiarPassword(Request $request, $id)
+    {
+        $usuario = User::find($id);
+
+        if($usuario)
+        {
+            $validacion = Validator::make(
+                $request->all(),
+                [
+                    'password' => 'required|string|min:8',
+                    'new_password' => 'required|string|min:8|confirmed',
+                ],
+                [
+                    'password.required' => 'La contraseña es requerida',
+                    'password.string' => 'La contraseña debe ser una cadena de texto',
+                    'password.min' => 'La contraseña debe tener como mínimo 8 caracteres',
+
+                    'new_password.required' => 'La nueva contraseña es requerida',
+                    'new_password.string' => 'La nueva contraseña debe ser una cadena de texto',
+                    'new_password.min' => 'La nueva contraseña debe tener como mínimo 8 caracteres',
+                    'new_password.confirmed' => 'Las contraseñas no coinciden',
+                ]
+            );
+
+            if ($validacion->fails()) {
+                return response()->json([
+                    'message' => $validacion->errors(),
+                ], 400);
+            }
+
+            else
+            {
+                if(Hash::check($request->password, $usuario->password))
+                {
+                    $usuario->password = Hash::make($request->new_password);
+
+                    if($usuario->save())
+                    {
+                        return response()->json([
+                            'message' => 'Cuenta editada'
+                        ], 200);
+                    }
+
+                    else
+                    {
+                        return response()->json([
+                            'message' => 'Error al editar la cuenta'
+                        ], 500);
+                    }
+                }
+
+                else
+                {
+                    return response()->json([
+                        'message' => 'Contraseña incorrecta'
+                    ], 400);
+                }
+            }
+        }
+
+        else
+        {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+    }
 }
